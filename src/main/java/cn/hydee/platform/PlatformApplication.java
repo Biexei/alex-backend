e4dd9c954f3f848de141875990daed6d81aca52d
xiebei <biexei@163.cocm>
2020-04-22 17:20:37 +0800

    first

---
+++ src/main/java/cn/hydee/platform/PlatformApplication.java
@@ -0,0 +1,13 @@
+package cn.hydee.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformApplication.class, args);
+    }
+
+}
