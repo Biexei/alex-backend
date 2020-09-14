28979f95ae6f890fa8eb433c7f9df2df1476c7f7
biexei@163.com <xiebei123>
2020-09-14 18:38:39 +0800

    修改过滤器

--- src/main/java/org/alex/platform/filter/CorsFilter.java
+++ src/main/java/org/alex/platform/filter/CorsFilter.java
@@ -21,25 +21,12 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         HttpServletRequest request = (HttpServletRequest) servletRequest;
 
-        // 不使用*，自动适配跨域域名，避免携带Cookie时失效
-        String origin = request.getHeader("Origin");
-        if(StringUtils.isNotBlank(origin)) {
-            response.setHeader("Access-Control-Allow-Origin", origin);
-        }
-
-        // 自适应所有自定义头
-        String headers = request.getHeader("Access-Control-Request-Headers");
-        if(StringUtils.isNotBlank(headers)) {
-            response.setHeader("Access-Control-Allow-Headers", headers);
-            response.setHeader("Access-Control-Expose-Headers", headers);
-        }
-
-        // 允许跨域的请求方法类型
-        response.setHeader("Access-Control-Allow-Methods", "*");
-        // 预检命令（OPTIONS）缓存时间，单位：秒
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST,HEAD,PUT,DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        // 明确许可客户端发送Cookie，不允许删除字段即可
+        response.setHeader("Access-Control-Allow-Headers", "Accept,Origin,X-Requested-With,Content-Type,X-Auth-Token");
         response.setHeader("Access-Control-Allow-Credentials", "true");
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override
